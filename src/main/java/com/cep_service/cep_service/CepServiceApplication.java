e3c8691aeba7b19a17381a09ed672f76a1fc40cf
Gabriel <oliveirafrerreira97@hotmail.com>
2025-11-04 19:53:43 -0300

    chore: inicializa projeto Spring Boot com H2 e estrutura base

---
+++ src/main/java/com/cep_service/cep_service/CepServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cep_service.cep_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cep_service/cep_service/CepServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cep_service.cep_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
