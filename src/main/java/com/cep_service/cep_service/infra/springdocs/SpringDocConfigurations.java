1c0adea64225f47beddf5cc977f5f6cb9b376ded
Gabriel <oliveirafrerreira97@hotmail.com>
2025-11-04 23:51:45 -0300

    fetc: adição da classe springdocsconfigration

---
+++ src/main/java/com/cep_service/cep_service/infra/springdocs/SpringDocConfigurations.java
@@ -0,0 +1,30 @@
+package com.cep_service.cep_service.infra.springdocs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cep-Service") // <-- Nome da sua API aqui
+                        .version("1.0.0")
+                        .description("API de informações de CEPs")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
