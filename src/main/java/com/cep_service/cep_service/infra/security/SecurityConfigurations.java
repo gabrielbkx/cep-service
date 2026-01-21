56abf2a1a3064d03a27885dd7a66af1ae02f7cca
Gabriel <oliveirafrerreira97@hotmail.com>
2026-01-20 23:08:12 -0300

    feat: adicionado o modulo do security

---
+++ src/main/java/com/cep_service/cep_service/infra/security/SecurityConfigurations.java
@@ -0,0 +1,25 @@
+package com.cep_service.cep_service.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http// Desliga proteção contra ataques de formulário (não precisa em API REST)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
+                    req.requestMatchers("/login").permitAll();
+                    req.anyRequest().authenticated(); // Todo o resto precisa de pulseira
+                })
+                .build();
+    }
+}
--- src/main/resources/db/migration/V1__create_table_ceps.sql
+++ src/main/resources/db/migration/V1__create_table_ceps.sql
@@ -1,5 +1,5 @@
 CREATE TABLE ceps (
-                      id SERIAL PRIMARY KEY,
+                      id BIGSERIAL PRIMARY KEY,
                       numero_cep VARCHAR(20) NOT NULL,
                       logradouro VARCHAR(255),
                       cidade VARCHAR(100)
